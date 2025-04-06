b9fffce2733902231ae33a39993437dd832e56d3
Pedro Bergamini <88423613+PedroSBergamini@users.noreply.github.com>
2025-04-06 18:53:21 -0300

    start
    first functional version

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,25 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "CHEGARAM OS GRANDES VILÕES DO JOGO",
+    "description": "confira as habilidades de Thanos e Galactus",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWb482cNoUrb1mDwhsd3FlBMfcbovud13Rzg&usqp=CAU"
+  },
+  {
+    "id":"2",
+    "title": "Confira os meta decks",
+    "description": "Galactus continua sendo um dos decks mais fortes, veja os outros desafiantes",
+    "photoCover":"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgpzkCNjtvjuZa1gy9YAiLKlK5qBPADlDBrrhaW8O4PN7OWK5h9i5PSY9llgAdDut1UmgFbyidpWu0yRoQ9yH5HH9xU6VeDEbIbni34yQZU_26vA0a4Chs91GRSZGQ9wHaJ_vRpsIiKUnJ2mizLWAFZxugY1xzX8NXgtKRrUP7u-Jo18wj5imDkyCbU5w/s2038/BinuisQ4J5L4vQuEa8uuwU.jpg"
+  },
+  { "id":"3",
+    "title": "Entrevista com criador do jogo",
+    "description": "Ben Brode nos contou os desafio para desenvolver o marvel sna",
+    "photoCover":"https://youre.outof.games/media/uploads/8f/54/8f544cd5-cceb-42a6-bbaa-ab4e40504ecb/brode-quicksilver-mulligan-insights.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Confira o que  a temporada de guardiões da galaxia reserva",
+    "description": "Veja a trilha de recompensa desse mês",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5lp6Dj995tTxKmyTaX2OkgDLI7uYi8bDlng&usqp=CAU"
+  }
+]
